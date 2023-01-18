82ec524c4a3474940ca37a47af345b5171bb92f5
Roshan Prajapat <roshanprajapat@fynd.com>
2023-01-18 11:56:34 +0530

    chore: radio story book

--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -66,56 +66,57 @@ export const RadioPlayground = Template.bind({});
 
 RadioPlayground.args = {
   labelText: "React",
-  value: "React",
-  id: "React",
-  name: "basic",
+  value: "React0",
+  id: "React0",
+  name: "basic0",
+  radioValue: "React0",
 };
 
-// export const radioSample = (args: RadioProps) => {
-//   const [selected, setSelected] = useState({ selectedRadio: "female" });
-//   const SelectedchangeHandler = (arg: any) => {
-//     setSelected({ ...selected, selectedRadio: arg });
-//   };
+export const radioSample = (args: RadioProps) => {
+  const [selected, setSelected] = useState({ selectedRadio: "girl" });
+  const SelectedchangeHandler = (arg: any) => {
+    setSelected({ ...selected, selectedRadio: arg });
+  };
 
-//   return (
-//     <div className="main-div space-between">
-//       <Radio
-//         labelText="Female"
-//         value="female"
-//         id="female"
-//         radioValue={selected.selectedRadio}
-//         onChange={SelectedchangeHandler}
-//         name="basic"
-//         showIcon={true}
-//         icon={SvgArray.SvgGirl}
-//         onIconClick={() => {
-//           console.log("I am a women");
-//         }}
-//       />
-//       <Radio
-//         labelText="Male"
-//         value="male"
-//         id="male"
-//         radioValue={selected.selectedRadio}
-//         onChange={SelectedchangeHandler}
-//         name="basic"
-//         showIcon={true}
-//         icon={SvgArray.SvgBoy}
-//         onIconClick={() => {
-//           console.log("I am a man");
-//         }}
-//       />
-//       <Radio
-//         labelText="Others"
-//         value="others"
-//         id="others"
-//         radioValue={selected.selectedRadio}
-//         onChange={SelectedchangeHandler}
-//         name="basic"
-//       />
-//     </div>
-//   );
-// };
+  return (
+    <div className="main-div space-between">
+      <Radio
+        labelText="Girl"
+        value="girl"
+        id="girl"
+        radioValue={selected.selectedRadio}
+        onChange={SelectedchangeHandler}
+        name="basic"
+        showIcon={true}
+        icon={SvgArray.SvgGirl}
+        onIconClick={() => {
+          console.log("I am a Girl");
+        }}
+      />
+      <Radio
+        labelText="Boy"
+        value="boy"
+        id="boy"
+        radioValue={selected.selectedRadio}
+        onChange={SelectedchangeHandler}
+        name="basic"
+        showIcon={true}
+        icon={SvgArray.SvgBoy}
+        onIconClick={() => {
+          console.log("I am a Boy");
+        }}
+      />
+      <Radio
+        labelText="Other"
+        value="other"
+        id="other"
+        radioValue={selected.selectedRadio}
+        onChange={SelectedchangeHandler}
+        name="basic"
+      />
+    </div>
+  );
+};
 
 export const stateSample = (args: RadioProps) => {
   const [selected, setSelected] = useState({ selectedRadio: "female" });
@@ -133,7 +134,7 @@ export const stateSample = (args: RadioProps) => {
         stateMessage="Success!"
         radioValue={selected.selectedRadio}
         onChange={SelectedchangeHandler}
-        name="basic"
+        name="basic2"
         showIcon={true}
         icon={SvgArray.SvgAutoMode}
       />
@@ -145,112 +146,69 @@ export const stateSample = (args: RadioProps) => {
         stateMessage="Warning!"
         radioValue={selected.selectedRadio}
         onChange={SelectedchangeHandler}
-        name="basic"
+        name="basic2"
         showIcon={true}
         icon={SvgArray.SvgAutoMode}
       />
       <Radio
-        labelText="Others"
-        value="others"
-        id="others"
+        labelText="Other"
+        value="other1"
+        id="other1"
         state="error"
         stateMessage="Error!"
         radioValue={selected.selectedRadio}
         onChange={SelectedchangeHandler}
-        name="basic"
+        name="basic2"
       />
     </div>
   );
 };
 
-export const radioVariants = (args: RadioProps) => {
+export const disabledRadioSample = (args: RadioProps) => {
+  const [selected, setSelected] = useState({ selectedRadio: "female1" });
+  const SelectedchangeHandler = (arg: any) => {
+    setSelected({ ...selected, selectedRadio: arg });
+  };
+
   return (
     <div className="main-div space-between">
-      <div className="n-badge-collection">
-        <div
-          style={{ display: "flex", flexDirection: "column", gap: "3.2rem" }}
-        >
-          <div style={{ display: "flex", gap: "3.2rem" }}>
-            <Radio labelText="Text" value="random1" id="random1" />
-
-            <Radio
-              labelText="Text"
-              value="random2"
-              id="random2"
-              radioValue={"random2"}
-            />
-          </div>
-
-          <div style={{ display: "flex", gap: "3.2rem" }}>
-            <Radio
-              labelText="Text"
-              value="randomValue1"
-              id="randomValue1"
-              disabled
-            />
-
-            <Radio
-              labelText="Text"
-              value="randomValue2"
-              id="randomValue2"
-              radioValue={"randomValue2"}
-              disabled
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export const radioSelectors = (args: RadioProps) => {
-  return (
-    <div className="main-div space-between">
-      <div className="n-badge-collection">
-        <div
-          style={{ display: "flex", flexDirection: "column", gap: "3.2rem" }}
-        >
-          <div style={{ display: "flex", gap: "3.2rem" }}>
-            <Radio
-              labelText="Add text here"
-              value="random1"
-              id="random1"
-              className="radio-story"
-              style={{ border: "1px solid #E0E0E0", background: "#FFFFFF" }}
-            />
-
-            <Radio
-              labelText="Add text here"
-              value="random2"
-              id="random2"
-              radioValue={"random2"}
-              className="radio-story"
-              style={{ background: "#E7EEFF", border: "1px solid #2E31BE" }}
-            />
-          </div>
-
-          <div style={{ display: "flex", gap: "3.2rem" }}>
-            <Radio
-              labelText="Add text here"
-              value="randomValue1"
-              id="randomValue1"
-              className="radio-story"
-              style={{ background: "#EDEDED", border: "1px solid #E0E0E0" }}
-              disabled
-            />
-
-            <Radio
-              labelText="Add text here"
-              value="randomValue2"
-              id="randomValue2"
-              radioValue={"randomValue2"}
-              className="radio-story"
-              style={{ border: "1px solid #2E31BE", background: "#E7EEFF" }}
-              disabled
-            />
-          </div>
-        </div>
-      </div>
+      <Radio
+        labelText="Female"
+        value="female1"
+        id="female1"
+        state="success"
+        stateMessage="Success!"
+        radioValue={selected.selectedRadio}
+        onChange={SelectedchangeHandler}
+        name="basic3"
+        disabled
+        showIcon={true}
+        icon={SvgArray.SvgAutoMode}
+      />
+      <Radio
+        labelText="Male"
+        value="male1"
+        id="male1"
+        state="warning"
+        stateMessage="Warning!"
+        radioValue={selected.selectedRadio}
+        onChange={SelectedchangeHandler}
+        name="basic3"
+        disabled
+        showIcon={true}
+        icon={SvgArray.SvgAutoMode}
+      />
+      <Radio
+        labelText="Other"
+        value="other2"
+        id="other2"
+        state="error"
+        stateMessage="Error!"
+        radioValue={selected.selectedRadio}
+        onChange={SelectedchangeHandler}
+        name="basic3"
+        disabled
+      />
     </div>
   );
 };
