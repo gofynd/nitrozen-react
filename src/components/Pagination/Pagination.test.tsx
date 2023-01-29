83e9fe62d2f9e3e9209fe0314071e1f77d5cb546
Amarjit Singh <amarjitsingh@MacBook-Pro-31.local>
2023-01-29 22:49:45 +0530

    test: adding test cases for onchange call

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-
 import Pagination, { ModeEnum } from "./Pagination";
 
 describe("Pagination", () => {
@@ -135,5 +134,111 @@ describe("Pagination", () => {
     fireEvent.click(dropdown);
 
     expect(dropdown.getAttribute("data-value")).toBe("20");
+  });
+
+  test("onchange should not have been called on initial render", () => {
+    const testProps = {
+      id: "1",
+      name: "Pages",
+      value: {
+        limit: 10,
+        total: 100,
+        current: 2,
+        prevPage: "",
+        nextPage: "",
+        currentPage: "",
+        currentTotal: 100,
+      },
+      pageSizeOptions: [10, 20],
+      onChange: jest.fn(),
+      onPreviousClick: (event: any) => {},
+      onNextClick: (event: any) => {},
+    };
+
+    render(<Pagination {...testProps} />);
+
+    expect(testProps?.onChange).toBeCalledTimes(0);
+  });
+
+  test("onchange should have been called on next click", () => {
+    const testProps = {
+      id: "1",
+      name: "Pages",
+      value: {
+        limit: 10,
+        total: 100,
+        current: 2,
+        prevPage: "",
+        nextPage: "",
+        currentPage: "",
+        currentTotal: 100,
+      },
+      pageSizeOptions: [10, 20],
+      onChange: jest.fn(),
+      onPreviousClick: (event: any) => {},
+      onNextClick: (event: any) => {},
+    };
+
+    const { getByTestId } = render(<Pagination {...testProps} />);
+
+    const nextBtn = getByTestId("btnNext");
+    fireEvent.click(nextBtn);
+
+    expect(testProps?.onChange).toBeCalledTimes(1);
+  });
+
+  test("onchange should have been called on prev click", () => {
+    const testProps = {
+      id: "1",
+      name: "Pages",
+      value: {
+        limit: 10,
+        total: 100,
+        current: 2,
+        prevPage: "",
+        nextPage: "",
+        currentPage: "",
+        currentTotal: 100,
+      },
+      pageSizeOptions: [10, 20],
+      onChange: jest.fn(),
+      onPreviousClick: (event: any) => {},
+      onNextClick: (event: any) => {},
+    };
+
+    const { getByTestId } = render(<Pagination {...testProps} />);
+
+    const prevBtn = getByTestId("btnPrevious");
+    fireEvent.click(prevBtn);
+
+    expect(testProps?.onChange).toBeCalledTimes(1);
+  });
+
+  test("onchange should have been called on changing pageSize", () => {
+    const testProps = {
+      id: "1",
+      name: "Pages",
+      value: {
+        limit: 10,
+        total: 100,
+        current: 2,
+        prevPage: "",
+        nextPage: "",
+        currentPage: "",
+        currentTotal: 100,
+      },
+      pageSizeOptions: [10, 20],
+      onChange: jest.fn(),
+      onPreviousClick: (event: any) => {},
+      onNextClick: (event: any) => {},
+    };
+
+    const screen = render(<Pagination {...testProps} />);
+
+    const dropdown =
+      screen.container.getElementsByClassName("nitrozen-option")[1];
+    fireEvent.click(dropdown);
+
+    expect(testProps?.onChange).toBeCalledTimes(1);
   });
 });
